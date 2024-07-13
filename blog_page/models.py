0bb3d64f78d2cf75e0789247cb86b6e550b6ae47
Hugo Felettig <hugo.felettig@gmail.com>
2024-07-13 18:06:24 +0200

    File for my first blog

---
+++ blog_page/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
